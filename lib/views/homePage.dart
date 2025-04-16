1e457ad0dc73e9c96b3a28b07691d35acb890f8f
RicharPena <rdpena521@gmail.com>
2025-04-16 13:27:41 -0500

    Se creó la pestaña o vista homePage

---
+++ lib/views/homePage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:wallet_safe/views/tabs/home_tab.dart';
+import 'package:wallet_safe/views/tabs/ingresos_tab.dart';
+import 'package:wallet_safe/views/tabs/gastos_tab.dart';
+import 'package:wallet_safe/views/tabs/estadisticas_tab.dart';
+import 'package:wallet_safe/views/tabs/config_tab.dart';
+import 'package:wallet_safe/widgets/barra_inferior.dart';
+
+class HomePage extends StatelessWidget {
+  final _navigatorKeys = List.generate(5, (_) => GlobalKey<NavigatorState>());
+
+  @override
+  Widget build(BuildContext context) {
+    return PersistentBottomBarScaffold(
+      items: [
+        PersistentTabItem(
+          tab: HomeTab(),
+          icon: Icons.home,
+          title: 'Inicio',
+          navigatorkey: _navigatorKeys[0],
+        ),
+        PersistentTabItem(
+          tab: IngresosTab(),
+          icon: Icons.savings, // ¡ícono tipo alcancía!
+          title: 'Ingresos',
+          navigatorkey: _navigatorKeys[1],
+        ),
+        PersistentTabItem(
+          tab: GastosTab(),
+          icon: Icons.money_off, // ícono que representa gasto
+          title: 'Gastos',
+          navigatorkey: _navigatorKeys[2],
+        ),
+        PersistentTabItem(
+          tab: EstadisticasTab(),
+          icon: Icons.bar_chart, // gráfico de barras
+          title: 'Estadísticas',
+          navigatorkey: _navigatorKeys[3],
+        ),
+        PersistentTabItem(
+          tab: ConfiguracionTab(),
+          icon: Icons.settings, // tuerca
+          title: 'Ajustes',
+          navigatorkey: _navigatorKeys[4],
+        ),
+      ],
+    );
+  }
+}

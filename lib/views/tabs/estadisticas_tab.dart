dfad9db02bdc41cee900f066aa54a4de170c0aef
RicharPena <rdpena521@gmail.com>
2025-04-16 13:25:04 -0500

    Se creo la carpeta tabs la cual contiene las ventanas de la home page

---
+++ lib/views/tabs/estadisticas_tab.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EstadisticasTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Estadísticas')),
+      body: Center(child: Text('Gráficos y estadísticas')),
+    );
+  }
+}

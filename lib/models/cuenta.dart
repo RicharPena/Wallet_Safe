844dfd48c4c586fcc799801a0927c1047b31645c
RicharPena <rdpena521@gmail.com>
2025-04-16 10:10:46 -0500

    Se añadió nuevo atributo de perfiles por consiguiente se modifico el constructor. También se modifico el metodo de iniciarSesion

--- lib/models/cuenta.dart
+++ lib/models/cuenta.dart
@@ -1,9 +1,20 @@
+import 'package:wallet_safe/models/perfil.dart';
+
 class Cuenta {
   final String name;
   final String email;
   final String password;
+  List<Perfil> perfiles = [];
 
-  Cuenta({required this.name, required this.email, required this.password});
+  //Variable global
+  static Cuenta? cuentaActiva;
+
+  Cuenta({
+    required this.name,
+    required this.email,
+    required this.password,
+    List<Perfil>? perfiles,
+  }) : perfiles = perfiles ?? [Perfil(id: 1, nombre: 'Titular')];
 
   // Simulación de base de datos
   static final List<Cuenta> _cuentas = [];
@@ -13,8 +24,20 @@ class Cuenta {
   }
 
   static bool iniciarSesion(String email, String password) {
-    return _cuentas.any((c) => c.email == email && c.password == password);
+    try {
+      final cuenta = _cuentas.firstWhere(
+        (c) => c.email == email && c.password == password,
+      );
+      cuentaActiva = cuenta;
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
+
+  /*
+   FALTAN LOS MÉTODOS DE CERRAR SESION Y EDITAR PERFIL
+  */
 
   //Tengamos esta por si acaso
   static Cuenta? obtenerCuenta(String email) {

cf9f3939d5ed4cc44a0ef2399d8818d48c42545d
RicharPena <rdpena521@gmail.com>
2025-04-16 13:24:27 -0500

    Se creo la carpeta tabs la cual contiene las ventanas de la home page

---
+++ lib/views/tabs/config_tab.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ConfiguracionTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Configuración')),
+      body: Center(child: Text('Opciones de la cuenta')),
+    );
+  }
+}

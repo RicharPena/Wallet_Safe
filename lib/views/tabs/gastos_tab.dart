d7af911e4d5d620c5ee1413d1c89a06858be11c8
RicharPena <rdpena521@gmail.com>
2025-04-16 13:25:40 -0500

    Se creo la carpeta tabs la cual contiene las ventanas de la home page

---
+++ lib/views/tabs/gastos_tab.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class GastosTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Agregar Gastos')),
+      body: Center(child: Text('Aquí van los gastos')),
+    );
+  }
+}

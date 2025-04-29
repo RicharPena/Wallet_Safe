6ee83bdcfab187cbdcf10bd6e8471c52c477cf26
RicharPena <rdpena521@gmail.com>
2025-04-29 17:49:59 -0500

    Se creó el controlador para los demas perfiles en home_tab

---
+++ lib/controllers/home_tab_oprofiles_cotroller.dart
@@ -0,0 +1,15 @@
+import 'package:fl_chart/fl_chart.dart';
+
+class PerfilController {
+  static List<FlSpot> getLineSpots() {
+    return [
+      FlSpot(0, 1),
+      FlSpot(1, 1.5),
+      FlSpot(2, 1.8),
+      FlSpot(3, 10),
+      FlSpot(4, -5),
+      FlSpot(5, 2),
+      FlSpot(6, 2.5),
+    ];
+  }
+}

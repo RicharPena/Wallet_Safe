a1bcb0cb93715556c423c7b7f17697de4cca35b4
RicharPena <rdpena521@gmail.com>
2025-04-29 17:49:19 -0500

    Se creó el controlador para el titular en home_tab

---
+++ lib/controllers/home_tab_titular_controller.dart
@@ -0,0 +1,17 @@
+import 'package:fl_chart/fl_chart.dart';
+
+class TitularController {
+  static List<FlSpot> getLineSpots() {
+    return [
+      FlSpot(0, 2),
+      FlSpot(1, 3),
+      FlSpot(2, 2.5),
+      FlSpot(3, 3),
+      FlSpot(4, 7),
+      FlSpot(5, 4),
+      FlSpot(6, 3.5),
+      FlSpot(7, 80),
+      FlSpot(8, -90),
+    ];
+  }
+}

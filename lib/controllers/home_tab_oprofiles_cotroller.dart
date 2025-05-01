d6a762243eca8f6c3fcc046eed30d4a9d9812492
RicharPena <rdpena521@gmail.com>
2025-05-01 16:32:00 -0500

    Se modificó para que cuando recibiera un nombre, generara datos aleatorios

--- lib/controllers/home_tab_oprofiles_cotroller.dart
+++ lib/controllers/home_tab_oprofiles_cotroller.dart
@@ -1,15 +1,16 @@
 import 'package:fl_chart/fl_chart.dart';
 
-class PerfilController {
-  static List<FlSpot> getLineSpots() {
-    return [
-      FlSpot(0, 1),
-      FlSpot(1, 1.5),
-      FlSpot(2, 1.8),
-      FlSpot(3, 10),
-      FlSpot(4, -5),
-      FlSpot(5, 2),
-      FlSpot(6, 2.5),
-    ];
+//Por ahora esta clase tomará los nombres y generará valores aleatorios
+class OProfilesController {
+  static List<FlSpot> getLineSpots(String nombrePerfil) {
+    // Usa una semilla derivada del nombre para generar valores distintos
+    final seed = nombrePerfil.codeUnits.fold(0, (prev, e) => prev + e);
+
+    return List.generate(7, (i) {
+      final y =
+          ((seed * i) % 15) -
+          5; // Genera un valor pseudoaleatorio entre -5 y 10
+      return FlSpot(i.toDouble(), y.toDouble());
+    });
   }
 }

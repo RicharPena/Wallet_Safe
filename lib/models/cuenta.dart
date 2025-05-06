5766491c4d43c8ba15b7a4d15a21883068342e94
RicharPena <rdpena521@gmail.com>
2025-05-05 21:46:13 -0500

    Se modifico la clase cuenta con el fin de manejar los metodos de cuenta_service

--- lib/models/cuenta.dart
+++ lib/models/cuenta.dart
@@ -1,45 +1,61 @@
 import 'package:wallet_safe/models/perfil.dart';
+import 'package:wallet_safe/models/titular.dart';
+import 'package:wallet_safe/models/familia.dart';
+import 'package:wallet_safe/services/cuenta_service.dart';
 
 class Cuenta {
   final String name;
   final String email;
   final String password;
-  List<Perfil> perfiles = [];
 
-  //Variable global
+  final List<Perfil> perfiles = [];
+
   static Cuenta? cuentaActiva;
 
-  Cuenta({
-    required this.name,
-    required this.email,
-    required this.password,
-    List<Perfil>? perfiles,
-  }) : perfiles = perfiles ?? [Perfil(id: 1, nombre: 'Titular')];
+  // Constructor: crea el Titular como el primer perfil
+  Cuenta({required this.name, required this.email, required this.password}) {
+    perfiles.add(Titular(id: 0, nombre: 'Titular'));
+  }
+
+  /// Obtener el titular
+  Titular get titular => perfiles.firstWhere((p) => p is Titular) as Titular;
+
+  /// Obtener todos los perfiles tipo Familia
+  List<Familia> get familiares =>
+      perfiles.whereType<Familia>().toList(growable: false);
+
+  /// Agregar un perfil tipo Familia
+  void agregarFamiliar(String nombreFamiliar) {
+    final nuevoFamiliar = Familia(id: perfiles.length, nombre: nombreFamiliar);
+    perfiles.add(nuevoFamiliar);
+  }
 
   // Simulación de base de datos
   static final List<Cuenta> _cuentas = [];
 
+  //Guarda los datos en la base de datos
   static void registrar(Cuenta cuenta) {
     _cuentas.add(cuenta);
   }
 
-  static bool iniciarSesion(String email, String password) {
-    try {
-      final cuenta = _cuentas.firstWhere(
-        (c) => c.email == email && c.password == password,
+  //MÉTODO PARA INICIAR SESION
+  static Future<Cuenta?> iniciarSesion(String correo, String contrasena) async {
+    final response = await CuentaService().login(correo, contrasena);
+
+    if (response['estado'] == 'ok') {
+      final usuario = response['usuario'];
+      final cuenta = Cuenta(
+        name: usuario['nombre'],
+        email: usuario['correo'],
+        password: contrasena,
       );
       cuentaActiva = cuenta;
-      return true;
-    } catch (e) {
-      return false;
+      return cuenta;
+    } else {
+      return null;
     }
   }
 
-  /*
-   FALTAN LOS MÉTODOS DE CERRAR SESION Y EDITAR PERFIL
-  */
-
-  //Tengamos esta por si acaso
   static Cuenta? obtenerCuenta(String email) {
     try {
       return _cuentas.firstWhere((c) => c.email == email);

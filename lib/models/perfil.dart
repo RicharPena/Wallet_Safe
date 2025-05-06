5edbc9e74bca102af02bb73d3dac248f76debc80
RicharPena <rdpena521@gmail.com>
2025-05-05 21:42:42 -0500

    Se modifico la clase perfil con el fin de manejar la lista de ingresos y gastos

--- lib/models/perfil.dart
+++ lib/models/perfil.dart
@@ -1,16 +1,19 @@
-class Perfil {
+import 'ingresos.dart';
+import 'gastos.dart';
+
+abstract class Perfil {
   final int id;
   final String nombre;
   double presupuestoFamiliar;
-
-  // List<Ingreso> cnIngresos = [];
-  // List<Gasto> cnGastos = [];
+  final List<Ingreso> cnIngresos;
+  final List<Gasto> cnGastos;
 
   Perfil({
     required this.id,
     required this.nombre,
     this.presupuestoFamiliar = 0.0,
-    // this.cnIngresos = const [],
-    // this.cnGastos = const [],
-  });
+    List<Ingreso>? cnIngresos,
+    List<Gasto>? cnGastos,
+  }) : cnIngresos = cnIngresos ?? [],
+       cnGastos = cnGastos ?? [];
 }

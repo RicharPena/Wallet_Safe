0159313fa4871e2f38b03d324070c0520c13122c
RicharPena <rdpena521@gmail.com>
2025-05-11 13:50:59 -0500

    se modifico para aceptar ingresos y gastos propios de perfiles titulares

--- lib/controllers/home_tab_titular_controller.dart
+++ lib/controllers/home_tab_titular_controller.dart
@@ -1,17 +1,44 @@
 import 'package:fl_chart/fl_chart.dart';
+import 'package:wallet_safe/models/perfil.dart';
+import 'package:wallet_safe/models/ingresos.dart';
+import 'package:wallet_safe/models/gastos.dart';
 
 class TitularController {
-  static List<FlSpot> getLineSpots() {
-    return [
-      FlSpot(0, 2),
-      FlSpot(1, 3),
-      FlSpot(2, 2.5),
-      FlSpot(3, 3),
-      FlSpot(4, 7),
-      FlSpot(5, 4),
-      FlSpot(6, 3.5),
-      FlSpot(7, 80),
-      FlSpot(8, -90),
-    ];
+  static List<FlSpot> getLineSpots(Perfil perfil, String vista) {
+    DateTime now = DateTime.now();
+
+    late List<DateTime> fechas;
+    switch (vista) {
+      case 'Semana':
+        fechas = List.generate(7, (i) => now.subtract(Duration(days: 6 - i)));
+        break;
+      case 'Mes':
+        fechas = List.generate(30, (i) => now.subtract(Duration(days: 29 - i)));
+        break;
+      case 'Hoy':
+      default:
+        fechas = [now];
+        break;
+    }
+
+    return fechas.asMap().entries.map((entry) {
+      int index = entry.key;
+      DateTime fecha = entry.value;
+
+      final ingresos = perfil.cnIngresos
+          .where((i) => _esMismaFecha(i.fecha, fecha))
+          .fold(0.0, (sum, i) => sum + i.monto);
+
+      final gastos = perfil.cnGastos
+          .where((g) => _esMismaFecha(g.fecha, fecha))
+          .fold(0.0, (sum, g) => sum + g.monto);
+
+      final balance = ingresos - gastos;
+      return FlSpot(index.toDouble(), balance);
+    }).toList();
+  }
+
+  static bool _esMismaFecha(DateTime a, DateTime b) {
+    return a.year == b.year && a.month == b.month && a.day == b.day;
   }
 }

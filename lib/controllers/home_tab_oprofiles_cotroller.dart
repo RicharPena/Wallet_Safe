e1b7bb9d19211d1397f02232e71364a70d2d0d77
RicharPena <rdpena521@gmail.com>
2025-05-11 14:00:40 -0500

    cambios importantes

--- lib/controllers/home_tab_oprofiles_cotroller.dart
+++ lib/controllers/home_tab_oprofiles_cotroller.dart
@@ -1,16 +1,42 @@
 import 'package:fl_chart/fl_chart.dart';
+import 'package:wallet_safe/models/perfil.dart';
 
-//Por ahora esta clase tomará los nombres y generará valores aleatorios
 class OProfilesController {
-  static List<FlSpot> getLineSpots(String nombrePerfil) {
-    // Usa una semilla derivada del nombre para generar valores distintos
-    final seed = nombrePerfil.codeUnits.fold(0, (prev, e) => prev + e);
+  static List<FlSpot> getLineSpots(Perfil perfil, String vista) {
+    DateTime now = DateTime.now();
 
-    return List.generate(7, (i) {
-      final y =
-          ((seed * i) % 15) -
-          5; // Genera un valor pseudoaleatorio entre -5 y 10
-      return FlSpot(i.toDouble(), y.toDouble());
-    });
+    late List<DateTime> fechas;
+    switch (vista) {
+      case 'Semana':
+        fechas = List.generate(7, (i) => now.subtract(Duration(days: 6 - i)));
+        break;
+      case 'Mes':
+        fechas = List.generate(30, (i) => now.subtract(Duration(days: 29 - i)));
+        break;
+      case 'Hoy':
+      default:
+        fechas = [now];
+        break;
+    }
+
+    return fechas.asMap().entries.map((entry) {
+      int index = entry.key;
+      DateTime fecha = entry.value;
+
+      final ingresos = perfil.cnIngresos
+          .where((i) => _esMismaFecha(i.fecha, fecha))
+          .fold(0.0, (sum, i) => sum + i.monto);
+
+      final gastos = perfil.cnGastos
+          .where((g) => _esMismaFecha(g.fecha, fecha))
+          .fold(0.0, (sum, g) => sum + g.monto);
+
+      final balance = ingresos - gastos;
+      return FlSpot(index.toDouble(), balance);
+    }).toList();
+  }
+
+  static bool _esMismaFecha(DateTime a, DateTime b) {
+    return a.year == b.year && a.month == b.month && a.day == b.day;
   }
 }

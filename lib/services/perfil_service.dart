ddc3943b57f599bd057feab273d5bd8cf152e3ec
RicharPena <rdpena521@gmail.com>
2025-06-17 09:45:46 -0500

    Modificado método del familiaViewModelProvider

--- lib/services/perfil_service.dart
+++ lib/services/perfil_service.dart
@@ -7,7 +7,7 @@ import 'package:wallet_safe/models/familia.dart'; // Importar Familia
 
 class PerfilService {
   final String baseUrl =
-      'http://192.168.1.65/proyecto_wallet_safe/controlador/broker.php';
+      'http://172.16.17.227/proyecto_wallet_safe/controlador/broker.php';
 
   // --- Métodos para Autenticación y Obtención de Perfiles ---
 
